58a41446e00df0fdf683a7fa31a5f8d8adce1325
Carlos Guerrero Castro <Carlos Guerrero Castro>
2017-02-28 22:31:15 -0600

    Styling login layout

--- app/src/main/java/com/platzi/test/platzigram/LoginActivity.java
+++ app/src/main/java/com/platzi/test/platzigram/LoginActivity.java
@@ -2,6 +2,9 @@ package com.platzi.test.platzigram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final TextView createAccountBtn = (TextView) findViewById(R.id.createHere);
+
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(v);
+            }
+        });
+    }
+    public void createAccount(View view){
+
     }
 }
