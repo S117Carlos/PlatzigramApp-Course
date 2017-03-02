b79a2489e59ea2f5a301df989f10c4bf0f709d9d
Carlos Guerrero Castro <Carlos Guerrero Castro>
2017-03-01 21:26:53 -0600

    Add Create Account layout, toolbar, upButton in toolbar and a reference layout for toolbar

--- app/src/main/java/com/platzi/test/platzigram/LoginActivity.java
+++ app/src/main/java/com/platzi/test/platzigram/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.platzi.test.platzigram;
 
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.platzi.test.platzigram.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
     public void createAccount(View view){
-
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/platzi/test/platzigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/platzi/test/platzigram/view/CreateAccountActivity.java
@@ -2,8 +2,11 @@ package com.platzi.test.platzigram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.platzi.test.platzigram.R;
+
+import java.util.zip.Inflater;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -11,5 +14,12 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_title_create_account),true);
+    }
+    public void showToolbar(String title,boolean upBtn){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upBtn);
     }
 }
