4530db368fd84efee952fa44bbac5c92e4cbf92c
Carlos Guerrero Castro <Carlos Guerrero Castro>
2017-03-02 23:24:21 -0600

    Add fragments and bottom bar layouts

---
+++ app/src/main/java/com/platzi/test/platzigram/view/ContainerActivity.java
@@ -0,0 +1,15 @@
+package com.platzi.test.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.platzi.test.platzigram.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
