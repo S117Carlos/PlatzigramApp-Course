5c252480fd9524a29369fd2d34b2074110d0234d
Carlos Guerrero Castro <Carlos Guerrero Castro>
2017-03-03 21:39:32 -0600

    Switch between tabs in bottom bar and and click on login

--- app/src/main/java/com/platzi/test/platzigram/LoginActivity.java
+++ app/src/main/java/com/platzi/test/platzigram/LoginActivity.java
@@ -5,9 +5,11 @@ import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.platzi.test.platzigram.view.ContainerActivity;
 import com.platzi.test.platzigram.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,16 +19,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         final TextView createAccountBtn = (TextView) findViewById(R.id.createHere);
-
+        final Button login = (Button) findViewById(R.id.login);
         createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 createAccount(v);
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(v);
+            }
+        });
+
     }
     public void createAccount(View view){
         Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/platzi/test/platzigram/view/ContainerActivity.java
+++ app/src/main/java/com/platzi/test/platzigram/view/ContainerActivity.java
@@ -1,9 +1,17 @@
 package com.platzi.test.platzigram.view;
 
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.platzi.test.platzigram.R;
+import com.platzi.test.platzigram.view.fragments.HomeFragment;
+import com.platzi.test.platzigram.view.fragments.ProfileFragment;
+import com.platzi.test.platzigram.view.fragments.SearchFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -11,5 +19,36 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottombar);
+        bottomBar.setDefaultTab(R.id.home);
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId){
+                    case R.id.home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.profile:
+
+                        ProfileFragment profileFragment = new ProfileFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.search:
+
+                        SearchFragment searchFragment = new SearchFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,searchFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                }
+            }
+        });
     }
 }
